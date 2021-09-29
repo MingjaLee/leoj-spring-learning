8214ae3f7174cda303ee40cb2650738f71490594
一弓一土两亩田 <775848118@qq.com>
2021-09-29 23:59:39 +0800

    alias 标签

--- spring-core-demo/src/main/java/com/naocn/leoj/spring/core/demo/OneBeanExample.java
+++ spring-core-demo/src/main/java/com/naocn/leoj/spring/core/demo/OneBeanExample.java
@@ -9,5 +9,9 @@ public class OneBeanExample {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring/one-bean.xml");
         Leaf leaf = context.getBean("leaf", Leaf.class);
         leaf.sayHello();
+
+        // <alias> example
+        Leaf leaf1 = context.getBean("leaf1", Leaf.class);
+        leaf1.sayHello();
     }
 }

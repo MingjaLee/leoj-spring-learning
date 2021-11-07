ccace190c0b46eb2cb8d3acbb0d18e6da01de7cd
一弓一土两亩田 <775848118@qq.com>
2021-11-07 21:54:01 +0800

    spring-log-demo: bridge log4j -> logback

---
+++ spring-log-demo/spring-log4j-bridge-logback-demo/src/main/java/com/naocn/leoj/spring/logbridge/demo/Application.java
@@ -0,0 +1,28 @@
+package com.naocn.leoj.spring.logbridge.demo;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+@SpringBootApplication
+public class Application {
+    private static final Logger log = Logger.getLogger(Application.class);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        int length = context.getBeanDefinitionNames().length;
+        log.trace("Spring boot启动初始化了 {} 个 Bean"+length);
+        log.debug("Spring boot启动初始化了 {} 个 Bean"+length);
+        log.info("Spring boot启动初始化了 {} 个 Bean"+length);
+        log.warn("Spring boot启动初始化了 {} 个 Bean"+length);
+        log.error("Spring boot启动初始化了 {} 个 Bean"+length);
+        try {
+            int i = 0;
+            int j = 1 / i;
+        } catch (Exception e) {
+            log.error("【SpringBootDemoLogbackApplication】启动异常：", e);
+        }
+    }
+}
